c7970a4595d1943d7e6e2ffbfc00f178d9dd0b5b
Pedro Schmitz <88664287+schmitz014@users.noreply.github.com>
2023-08-03 09:55:44 -0300

    Creating data for the components

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -5,6 +5,32 @@ import { Component } from '@angular/core';
   templateUrl: './first-component.component.html',
   styleUrls: ['./first-component.component.css']
 })
-export class FirstComponentComponent {
 
+export class FirstComponentComponent {
+  name: string = 'Peter'
+  age: number = 25
+  job: string = 'Developer'
+  hobbies: string[] = ['Sports', ' Cooking']
+  address: Address = {
+    street: '50 Main st',
+    city: 'Boston',
+    state: 'MA'
+  }
+  car: Car = {
+    brand: 'Ford',
+    model: 'Mustang',
+    year: 1964
+  }
+}
+
+interface Address {
+  street: string,
+  city: string,
+  state: string
+}
+
+interface Car {
+  brand: string,
+  model: string,
+  year: number
 }
