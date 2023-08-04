af2eafdd5db15a155cb82a0490da659a60fe81e3
Pedro Schmitz <88664287+schmitz014@users.noreply.github.com>
2023-08-03 22:03:15 -0300

    Sharign data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'Project-Angular';
+  userName = 'John'
+  userData = {
+    email: 'john@gmail.com',
+    age: 25,
+    address: 'USA',
+    phone: '1234567890',
+    role: 'Admin'
+  }
+  title = 'Project-Angular'
 }
