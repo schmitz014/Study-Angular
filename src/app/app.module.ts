ab904200857c5bcc0c71d32630f43e1f8028c95a
Pedro Schmitz <88664287+schmitz014@users.noreply.github.com>
2023-08-08 14:47:35 -0300

    Two way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +13,7 @@ import { EmitterComponent } from './components/emitter/emitter.component';
 import { ChangeNumberComponent } from './components/change-number/change-number.component';
 import { ListRenderComponent } from './components/list-render/list-render.component';
 import { PipeComponent } from './components/pipe/pipe.component';
+import { TwoWayBindingComponent } from './components/two-way-binding/two-way-binding.component';
 
 @NgModule({
   declarations: [
@@ -24,11 +26,13 @@ import { PipeComponent } from './components/pipe/pipe.component';
     EmitterComponent,
     ChangeNumberComponent,
     ListRenderComponent,
-    PipeComponent
+    PipeComponent,
+    TwoWayBindingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
