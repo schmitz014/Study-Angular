f4ac9e1b8bb425a18c8a0a3b9dceb9bf3ae9f8c3
Pedro Schmitz <88664287+schmitz014@users.noreply.github.com>
2023-08-09 09:25:28 -0300

    Routing components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
+import { IfRenderComponent } from './components/if-render/if-render.component';
+
+const routes: Routes = [
+  {path: '', component: FirstComponentComponent},
+  {path: 'list', component: ListRenderComponent},
+  {path: 'if', component: IfRenderComponent}
+];
 
 @NgModule({
+  declarations: [],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
